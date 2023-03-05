f5984f3b0b33d4afb03ad3c30bff303014062fec
phueb <info@philhuebner.com>
2023-03-04 17:34:39 -0700

    add EmptyQueryResults

--- datascience_batch_job_utils/exceptions.py
+++ datascience_batch_job_utils/exceptions.py
@@ -1,6 +1,21 @@
 from typing import Optional
 
 
+class EmptyQueryResults(Exception):
+    """
+    Exception raised when SQL queries return an empty dataframe.
+    """
+
+    def __init__(self, fn_name: Optional[str] = None):
+        self.msg = f"SQL Query results are empty"
+        if fn_name is not None:
+            self.msg += f' for function {fn_name}'
+        super().__init__(self.msg)
+
+    def __str__(self):
+        return self.msg
+
+
 class SheetParsingError(Exception):
     """
     Exception raised when a Google Sheet from the SEO team cannot be parsed.
